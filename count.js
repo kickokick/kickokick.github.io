6eecbbad7256ae417bcdfdfe524ee5d69caee6a9
Kickokick <63968144+kickokick@users.noreply.github.com>
2024-06-16 12:57:12 -0400

    fix v5

--- count.js
+++ count.js
@@ -1,9 +1,10 @@
 
-var count = 0;
-let counter = document.getElementById("change");
+let count = 0;
+let text = "Count: " + count;
+const counter = document.getElementById("change");
 function Click(){
     count = count + 1;
-    var text = "Count: " + count;
+    text = "Count: " + count;
     console.log(text);
-    counter.appendChild(text);
+    counter.textContent = text;
 }
