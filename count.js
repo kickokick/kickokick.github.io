426585988a1e8c345a48b6b15273c57fffcaf03a
Kickokick <63968144+kickokick@users.noreply.github.com>
2024-06-16 13:19:45 -0400

    auto v1

--- count.js
+++ count.js
@@ -1,10 +1,28 @@
 
 let count = 0;
+let autoCount = 0;
 let text = "Count: " + count;
 const counter = document.getElementById("change");
+const actionText = document.getElementById("action");
 function Click(){
     count = count + 1;
     text = "Count: " + count;
     console.log(text);
     counter.textContent = text;
+    actionText.textContent = "You clicked";
 }
+
+function Buy(){
+    if (count > 10){
+        count = count - 10;
+        autoCount = autoCount + 1;
+        actionText.textContent = "You bought an auto clicker";
+    } else {
+        actionText.textContent = "sorry pal ur too broke";
+    }
+}
+
+const interval = setInterval(function(){
+    count = count + (autoCount*0.1);
+    counter.textContent = text;
+}, 1000);
