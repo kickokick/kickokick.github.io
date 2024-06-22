db8af3ecc3adb6132ea4c62d8085fe041a9e1794
GoodMorning9000 <seth.l.noel@gmail.com>
2024-06-22 18:25:26 -0400

    added some graphics
    pixi.js

---
+++ JS/graphics.js
@@ -0,0 +1,14 @@
+const app = new PIXI.Application();
+await app.init({ width: 640, height: 360 });
+app.canvas.id = "app";
+document.body.appendChild(app.canvas);
+
+await PIXI.Assets.load('./sample.png');
+let sprite = PIXI.Sprite.from('./sample.png');
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+app.ticker.add((ticker) => {
+    elapsed += ticker.deltaTime;
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
