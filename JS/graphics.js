b7e472f0c5136b8c31124b20fc9d04db4e79459f
Kickokick <63968144+kickokick@users.noreply.github.com>
2024-06-23 12:00:33 -0400

    cookies (commented out)

--- JS/graphics.js
+++ JS/graphics.js
@@ -8,5 +8,5 @@ app.stage.addChild(sprite);
 let elapsed = 0.0;
 app.ticker.add((ticker) => {
     elapsed += ticker.deltaTime;
-    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+    sprite.x = 10.0 + Math.cos(elapsed/50.0) * 50.0;
 });
