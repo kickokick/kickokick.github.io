e6b41d77b981e4e62085a1afbba107ecea663d57
GoodMorning9000 <seth.l.noel@gmail.com>
2024-06-24 00:36:39 -0400

    removed harishs bullshit

--- JS/combat.js
+++ JS/combat.js
@@ -1,63 +1,35 @@
 let catTick = 50;
 let enemyTick = 100;
-let currentEnemyFull = "";
-let currentEnemyCut = "";
-let currentEnemy = "";
 
 let currentEnemyData = {
+    name: null,
     health: null,
     damage: null,
     haste: null
 };
 
-
-function changeEnemy(name){
-    currentEnemyFull = document.getElementById("enemy");
-
-        currentEnemyFull.innerHTML = `Current Enemy: ${name}`;
-
-        currentEnemyData.health = enemies[name].health;
-        currentEnemyData.damage = enemies[name].damage;
-        currentEnemyData.haste = enemies[name].haste;
-
-
+function changeEnemy(name) {
+    document.getElementById("enemy").innerHTML = `Current Enemy: ${name}`;
+    
+    currentEnemyData.name = name;
+    currentEnemyData.health = enemies[name].health;
+    currentEnemyData.damage = enemies[name].damage;
+    currentEnemyData.haste = enemies[name].haste;
 }
 
-
-function getCurrentEnemy(){
-    currentEnemyFull = document.getElementById("enemy");
-
-    if (!(currentEnemyFull.innerHTML.lastIndexOf(": ") == -1)){
-        currentEnemyCut = currentEnemyFull.innerHTML.substring(currentEnemyFull.innerHTML.lastIndexOf(": ") + 2);
-    }
-
-    return currentEnemyCut;
+function resetEnemy() {
+    currentEnemyData.health = enemies[currentEnemyData.name].health;
+    currentEnemyData.damage = enemies[currentEnemyData.name].damage;
+    currentEnemyData.haste = enemies[currentEnemyData.name].haste;
 }
-
-
-function resetEnemy(){
-    currentEnemy = getCurrentEnemy();
-    if (currentEnemy == "Catipillar"){
-
-        currentEnemyData.health = enemies.catipillar.health;
-        currentEnemyData.damage = enemies.catipillar.damage;
-        currentEnemyData.haste = enemies.catipillar.haste;
-        console.log(currentEnemyData);
-        console.log(enemies.catipillar);
-
-    }
-}
-
 
 const combat = setInterval(function() {
+    actionText.innerHTML = currentEnemyData.name + " Health: " + currentEnemyData.health;
 
-    getCurrentEnemy();
-    actionText.innerHTML = getCurrentEnemy() + " Health: " + currentEnemyData.health;
     if (currentEnemyData.health <= 0){
         resetEnemy();
         console.log("Congrats you killed an innocent something");
     }
-
     if (!(catTick == 0)) {
         catTick -= catStats.haste;
         console.log(catTick);
--- JS/graphics.js
+++ JS/graphics.js
@@ -2,17 +2,31 @@ const app = new PIXI.Application();
 await app.init({ width: 1080, height: 360, canvas: document.getElementById("app") });
 
 await PIXI.Assets.load('./assets/cat.png');
-let sprite = PIXI.Sprite.from('./assets/cat.png');
+await PIXI.Assets.load('./assets/Catipillar.png');
+
+let cat = PIXI.Sprite.from('./assets/cat.png');
 app.stage.addChild(sprite);
 
-await PIXI.Assets.load('./assets/Catipillar.png');
-let enem = PIXI.Sprite.from('./assets/Catipillar.png');
-app.stage.addChild(enem);
+let enemy = PIXI.Sprite.from('./assets/Catipillar.png');
+app.stage.addChild(enemy);
 
 let elapsed = 0.0;
 app.ticker.add((ticker) => {
     elapsed += ticker.deltaTime;
-    sprite.x = 50.0 + Math.cos(elapsed/50.0) * 50.0;
-    enem.x = 700 + Math.sin(elapsed/50.0) * 50.0;
-    enem.y = 100
+    cat.x = 50.0 + Math.cos(elapsed/50.0) * 50.0;
+    enemy.x = 700 + Math.sin(elapsed/50.0) * 50.0;
+    enemy.y = 100
 });
+
+function catAttack() {
+
+}
+function enemyAttack() {
+    
+}
+function catDeath() {
+
+}
+function enemyDeath() {
+    
+}
